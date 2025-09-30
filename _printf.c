571981c510986807d2fe6fa0f70fd778f58c9f46
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-09-30 14:01:32 +0000

    Add _printf.c

--- _printf.c
+++ _printf.c
@@ -1,12 +1,52 @@
 i#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _printf - produces output according to a format
+ * @format: character string with directives
  *
- * Return: On success 1, -1 on error
+ * Return: number of characters printed (excluding null byte)
  */
-int _putchar(char c)
+int _printf(const char *format, ...)
 {
-  return (write(1, &c, 1));
+  va_list args;
+  int i = 0, count = 0;
+  char *str;
+
+  if (!format)
+    return (-1);
+
+  va_start(args, format);
+
+  while (format[i])
+  {
+    if (format[i] == '%')
+    {
+      i++;
+      if (!format[i])
+        break;
+      if (format[i] == 'c')
+        count += _putchar(va_arg(args, int));
+      else if (format[i] == 's')
+      {
+        str = va_arg(args, char *);
+        if (!str)
+          str = "(null)";
+        while (*str)
+          count += _putchar(*str++);
+      }
+      else if (format[i] == '%')
+        count += _putchar('%');
+      else
+      {
+        count += _putchar('%');
+        count += _putchar(format[i]);
+      }
+    }
+    else
+      count += _putchar(format[i]);
+    i++;
+  }
+
+  va_end(args);
+  return (count);
 }
