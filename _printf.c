1bca8ebda18b926208511e22b56b373a8adf95c0
Ali Abdullah <aliabdullah1215@icloud.com>
2025-09-30 14:36:58 +0000

    Fix: improve _printf to handle NULL, %c, %s, %% and errors

--- _printf.c
+++ _printf.c
@@ -4,49 +4,52 @@
  * _printf - produces output according to a format
  * @format: character string with directives
  *
- * Return: number of characters printed (excluding null byte)
+ * Return: number of characters printed (excluding null byte),
+ *         or -1 if error
  */
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int i = 0, count = 0;
-  char *str;
+	va_list args;
+	int i = 0, count = 0;
+	char *str;
 
-  if (!format)
-    return (-1);
+	if (format == NULL)
+		return (-1);
 
-  va_start(args, format);
+	va_start(args, format);
 
-  while (format[i])
-  {
-    if (format[i] == '%')
-    {
-      i++;
-      if (!format[i])
-        break;
-      if (format[i] == 'c')
-        count += _putchar(va_arg(args, int));
-      else if (format[i] == 's')
-      {
-        str = va_arg(args, char *);
-        if (!str)
-          str = "(null)";
-        while (*str)
-          count += _putchar(*str++);
-      }
-      else if (format[i] == '%')
-        count += _putchar('%');
-      else
-      {
-        count += _putchar('%');
-        count += _putchar(format[i]);
-      }
-    }
-    else
-      count += _putchar(format[i]);
-    i++;
-  }
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i]) /* case: % at end of string */
+				return (-1);
 
-  va_end(args);
-  return (count);
+			if (format[i] == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+					count += _putchar(*str++);
+			}
+			else if (format[i] == '%')
+				count += _putchar('%');
+			else
+			{
+				/* unknown specifier → print as is */
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+			count += _putchar(format[i]);
+		i++;
+	}
+
+	va_end(args);
+	return (count);
 }
