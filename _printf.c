3796e9695012a9801c88f5b219d367369e7cf9d9
Ali Abdullah <aliabdullah1215@icloud.com>
2025-10-01 13:26:37 +0000

    Update _printf to support %d and %i (task 1)

--- _printf.c
+++ _printf.c
@@ -9,28 +9,34 @@
  */
 int handle_specifier(char c, va_list args)
 {
-	if (c == 'c')
-		return (_putchar(va_arg(args, int)));
+        if (c == 'c')
+                return (_putchar(va_arg(args, int)));
 
-	if (c == 's')
-	{
-		char *str = va_arg(args, char *);
-		int count = 0;
+        if (c == 's')
+        {
+                char *str = va_arg(args, char *);
+                int count = 0;
 
-		if (str == NULL)
-			str = "(null)";
-		while (*str)
-			count += _putchar(*str++);
-		return (count);
-	}
+                if (str == NULL)
+                        str = "(null)";
+                while (*str)
+                        count += _putchar(*str++);
+                return (count);
+        }
 
-	if (c == '%')
-		return (_putchar('%'));
+        if (c == '%')
+                return (_putchar('%'));
 
-	/* Unknown specifier: print '%' and the character */
-	_putchar('%');
-	_putchar(c);
-	return (2);
+        if (c == 'd' || c == 'i')
+        {
+                int n = va_arg(args, int);
+                return (print_int(n));
+        }
+
+        /* Unknown specifier: print '%' and the character */
+        _putchar('%');
+        _putchar(c);
+        return (2);
 }
 
 /**
@@ -42,30 +48,31 @@ int handle_specifier(char c, va_list args)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, count = 0;
+        va_list args;
+        int i = 0, count = 0;
 
-	if (format == NULL)
-		return (-1);
+        if (format == NULL)
+                return (-1);
 
-	va_start(args, format);
+        va_start(args, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (!format[i])
-				return (-1);
-			count += handle_specifier(format[i], args);
-		}
-		else
-		{
-			count += _putchar(format[i]);
-		}
-		i++;
-	}
+        while (format[i])
+        {
+                if (format[i] == '%')
+                {
+                        i++;
+                        if (!format[i])
+                                return (-1);
+                        count += handle_specifier(format[i], args);
+                }
+                else
+                {
+                        count += _putchar(format[i]);
+                }
+                i++;
+        }
 
-	va_end(args);
-	return (count);
+        va_end(args);
+        return (count);
 }
+
