dd1f1189f8684d2663f75d96fedeefbc1db45069
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-02 08:40:29 +0000

    task0

--- _printf.c
+++ _printf.c
@@ -1,30 +1,52 @@
 #include "main.h"
 
 /**
- * print_int - prints an integer
- * @n: integer to print
+ * _printf - produces output according to a format
+ * @format: character string with directives
  *
- * Return: number of characters printed
+ * Return: number of characters printed (excluding null byte)
  */
-int print_int(int n)
+int _printf(const char *format, ...)
 {
-	int count = 0;
-	unsigned int num;
+  va_list args;
+  int i = 0, count = 0;
+  char *str;
 
-	if (n < 0)
-	{
-		count += _putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
+  if (!format)
+    return (-1);
 
-	if (num / 10)
-		count += print_int(num / 10);
+  va_start(args, format);
 
-	count += _putchar((num % 10) + '0');
+  while (format[i])
+  {
+    if (format[i] == '%')
+    {
+      i++;
+      if (!format[i])
+        break;
+      if (format[i] == 'c')
+        count += _putchar(va_arg(args, int));
+      else if (format[i] == 's')
+      {
+        str = va_arg(args, char *);
+        if (!str)
+          str = "(null)";
+        while (*str)
+          count += _putchar(*str++);
+      }
+      else if (format[i] == '%')
+        count += _putchar('%');
+      else
+      {
+        count += _putchar('%');
+        count += _putchar(format[i]);
+      }
+    }
+    else
+      count += _putchar(format[i]);
+    i++;
+  }
 
-	return (count);
+  va_end(args);
+  return (count);
 }
