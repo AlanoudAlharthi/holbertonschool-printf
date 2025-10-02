fc7c5c8854ba2b0b02403b9a001dde4d268d0154
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-02 08:38:07 +0000

    task0

--- _printf.c
+++ _printf.c
@@ -1,78 +1,30 @@
 #include "main.h"
 
 /**
- * handle_specifier - Chooses what to print based on format specifier
- * @c: The specifier character
- * @args: List of arguments
+ * print_int - prints an integer
+ * @n: integer to print
  *
- * Return: Number of characters printed
+ * Return: number of characters printed
  */
-int handle_specifier(char c, va_list args)
+int print_int(int n)
 {
-	if (c == 'c')
-		return (_putchar(va_arg(args, int)));
+	int count = 0;
+	unsigned int num;
 
-	if (c == 's')
+	if (n < 0)
 	{
-		char *str = va_arg(args, char *);
-		int count = 0;
-
-		if (str == NULL)
-			str = "(null)";
-		while (*str)
-			count += _putchar(*str++);
-		return (count);
+		count += _putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
 	}
 
-	if (c == '%')
-		return (_putchar('%'));
+	if (num / 10)
+		count += print_int(num / 10);
 
-	if (c == 'd' || c == 'i')
-	{
-		int n = va_arg(args, int);
+	count += _putchar((num % 10) + '0');
 
-		return (print_int(n));
-	}
-
-	/* Unknown specifier: print '%' and the character */
-	_putchar('%');
-	_putchar(c);
-	return (2);
-}
-
-/**
- * _printf - Produces output according to a format
- * @format: Character string with directives
- *
- * Return: Number of characters printed (excluding null byte),
- * or -1 if error occurs
- */
-int _printf(const char *format, ...)
-{
-	va_list args;
-	int i = 0, count = 0;
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(args, format);
-
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (!format[i])
-				return (-1);
-			count += handle_specifier(format[i], args);
-		}
-		else
-		{
-			count += _putchar(format[i]);
-		}
-		i++;
-	}
-
-	va_end(args);
 	return (count);
 }
