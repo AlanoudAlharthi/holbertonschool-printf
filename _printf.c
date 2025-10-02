55bd8aa7e5aeaaf98a5ff84c8ec3a9ee9345b82e
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-02 10:23:53 +0000

    task0

--- _printf.c
+++ _printf.c
@@ -1,52 +1,52 @@
 #include "main.h"
 
 /**
- * _printf - Produces output according to a format
- * @format: Character string with directives
+ * _printf - produces output according to a format
+ * @format: character string with directives
  *
- * Return: Number of characters printed (excluding null byte)
+ * Return: number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int i = 0, count = 0;
-	char *str;
+  va_list args;
+  int i = 0, count = 0;
+  char *str;
 
-	if (!format)
-		return (-1);
+  if (!format)
+    return (-1);
 
-	va_start(args, format);
+  va_start(args, format);
 
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			i++;
-			if (!format[i]) /* % is last character */
-				break;
-			if (format[i] == 'c')
-				count += _putchar(va_arg(args, int));
-			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(null)";
-				while (*str)
-					count += _putchar(*str++);
-			}
-			else if (format[i] == '%')
-				count += _putchar('%');
-			else
-			{
-				count += _putchar('%');
-				count += _putchar(format[i]);
-			}
-		}
-		else
-			count += _putchar(format[i]);
-		i++;
-	}
+  while (format[i])
+  {
+    if (format[i] == '%')
+    {
+      i++;
+      if (!format[i])
+        break;
+      if (format[i] == 'c')
+        count += _putchar(va_arg(args, int));
+      else if (format[i] == 's')
+      {
+        str = va_arg(args, char *);
+        if (!str)
+          str = "(null)";
+        while (*str)
+          count += _putchar(*str++);
+      }
+      else if (format[i] == '%')
+        count += _putchar('%');
+      else
+      {
+        count += _putchar('%');
+        count += _putchar(format[i]);
+      }
+    }
+    else
+      count += _putchar(format[i]);
+    i++;
+  }
 
-	va_end(args);
-	return (count);
+  va_end(args);
+  return (count);
 }
