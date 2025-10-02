71e73e5b79dcb1a4b8e70f0c4a36a7d03e158250
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-10-02 08:32:48 +0000

    task0

--- _printf.c
+++ _printf.c
@@ -1,78 +1,78 @@
 #include "main.h"
 
 /**
- * handle_specifier - chooses what to print based on format
- * @c: the specifier character
- * @args: list of arguments
+ * handle_specifier - Chooses what to print based on format specifier
+ * @c: The specifier character
+ * @args: List of arguments
  *
- * Return: number of characters printed
+ * Return: Number of characters printed
  */
 int handle_specifier(char c, va_list args)
 {
-        if (c == 'c')
-                return (_putchar(va_arg(args, int)));
+	if (c == 'c')
+		return (_putchar(va_arg(args, int)));
 
-        if (c == 's')
-        {
-                char *str = va_arg(args, char *);
-                int count = 0;
+	if (c == 's')
+	{
+		char *str = va_arg(args, char *);
+		int count = 0;
 
-                if (str == NULL)
-                        str = "(null)";
-                while (*str)
-                        count += _putchar(*str++);
-                return (count);
-        }
+		if (str == NULL)
+			str = "(null)";
+		while (*str)
+			count += _putchar(*str++);
+		return (count);
+	}
 
-        if (c == '%')
-                return (_putchar('%'));
+	if (c == '%')
+		return (_putchar('%'));
 
-        if (c == 'd' || c == 'i')
-        {
-                int n = va_arg(args, int);
-                return (print_int(n));
-        }
+	if (c == 'd' || c == 'i')
+	{
+		int n = va_arg(args, int);
 
-        /* Unknown specifier: print '%' and the character */
-        _putchar('%');
-        _putchar(c);
-        return (2);
+		return (print_int(n));
+	}
+
+	/* Unknown specifier: print '%' and the character */
+	_putchar('%');
+	_putchar(c);
+	return (2);
 }
 
 /**
- * _printf - produces output according to a format
- * @format: character string with directives
+ * _printf - Produces output according to a format
+ * @format: Character string with directives
  *
- * Return: number of characters printed (excluding null byte),
- * or -1 if error
+ * Return: Number of characters printed (excluding null byte),
+ * or -1 if error occurs
  */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int i = 0, count = 0;
+	va_list args;
+	int i = 0, count = 0;
 
-        if (format == NULL)
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (format[i])
-        {
-                if (format[i] == '%')
-                {
-                        i++;
-                        if (!format[i])
-                                return (-1);
-                        count += handle_specifier(format[i], args);
-                }
-                else
-                {
-                        count += _putchar(format[i]);
-                }
-                i++;
-        }
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i])
+				return (-1);
+			count += handle_specifier(format[i], args);
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
 
-        va_end(args);
-        return (count);
+	va_end(args);
+	return (count);
 }
-
