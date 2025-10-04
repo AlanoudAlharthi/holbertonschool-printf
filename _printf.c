4580e88578649004521f144db7a203438ca0995e
Ali Abdullah <aliabdullah1215@icloud.com>
2025-10-04 17:17:34 +0000

    Advanced: add 1024-char buffer to _printf for optimized write calls

--- _printf.c
+++ _printf.c
@@ -1,103 +1,125 @@
 #include "main.h"
 
 /**
- * print_string - prints a string
+ * print_string - prints a string to buffer
  * @s: string to print
+ * @buffer: buffer array
+ * @index: pointer to current buffer index
  *
  * Return: number of characters printed
  */
-int print_string(char *s)
+int print_string(char *s, char *buffer, int *index)
 {
-        int count = 0;
+	int count = 0;
 
-        if (s == NULL)
-                s = "(null)";
+	if (s == NULL)
+		s = "(null)";
 
-        while (*s)
-        {
-                count += _putchar(*s);
-                s++;
-        }
-
-        return (count);
+	while (*s)
+	{
+		print_buffer(buffer, index, *s);
+		count++;
+		s++;
+	}
+	return (count);
 }
 
 /**
  * handle_specifier - handles format specifiers
  * @c: specifier character
  * @args: list of arguments
+ * @buffer: buffer array
+ * @index: pointer to buffer index
  *
  * Return: number of characters printed
  */
-int handle_specifier(char c, va_list args)
+int handle_specifier(char c, va_list args, char *buffer, int *index)
 {
-        int count = 0;
+	int count = 0;
 
-        if (c == 'c')
-                count += _putchar(va_arg(args, int));
-        else if (c == 's')
-                count += print_string(va_arg(args, char *));
-        else if (c == '%')
-                count += _putchar('%');
-        else if (c == 'b')
-        {
-                long int val = va_arg(args, long int);
-                if (val < 0)
-                        val = (unsigned long int)val;
-                count += print_binary((unsigned long int)val);
-        }
-        else if (c == 'u')
-                count += print_unsigned(va_arg(args, unsigned int));
-        else if (c == 'o')
-                count += print_octal(va_arg(args, unsigned int));
-        else if (c == 'x')
-                count += print_hex(va_arg(args, unsigned int), 0);
-        else if (c == 'X')
-                count += print_hex(va_arg(args, unsigned int), 1);
-        else
-        {
-                count += _putchar('%');
-                count += _putchar(c);
-        }
+	switch (c)
+	{
+	case 'c':
+		print_buffer(buffer, index, va_arg(args, int));
+		count++;
+		break;
+	case 's':
+		count += print_string(va_arg(args, char *), buffer, index);
+		break;
+	case '%':
+		print_buffer(buffer, index, '%');
+		count++;
+		break;
+	case 'b':
+	{
+		long int val = va_arg(args, long int);
+		if (val < 0)
+			val = (unsigned long int)val;
+		count += print_binary((unsigned long int)val);
+		break;
+	}
+	case 'u':
+		count += print_unsigned(va_arg(args, unsigned int));
+		break;
+	case 'o':
+		count += print_octal(va_arg(args, unsigned int));
+		break;
+	case 'x':
+		count += print_hex(va_arg(args, unsigned int), 0);
+		break;
+	case 'X':
+		count += print_hex(va_arg(args, unsigned int), 1);
+		break;
+	default:
+		print_buffer(buffer, index, '%');
+		print_buffer(buffer, index, c);
+		count += 2;
+		break;
+	}
 
-        return (count);
+	return (count);
 }
 
 /**
- * _printf - produces output according to a format
+ * _printf - produces output according to a format using a 1024-char buffer
  * @format: character string with directives
  *
  * Return: number of characters printed (excluding null byte)
  */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int i = 0, count = 0;
+	va_list args;
+	int i = 0, count = 0;
+	char buffer[1024];
+	int index = 0;
 
-        if (format == NULL)
-                return (-1);
+	if (format == NULL)
+		return (-1);
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (format[i])
-        {
-                if (format[i] == '%')
-                {
-                        i++;
-                        if (format[i] == '\0')
-                        {
-                                va_end(args);
-                                return (-1);
-                        }
-                        count += handle_specifier(format[i], args);
-                }
-                else
-                {
-                        count += _putchar(format[i]);
-                }
-                i++;
-        }
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				print_buffer(buffer, &index, -1); /* flush before returning */
+				va_end(args);
+				return (-1);
+			}
+			count += handle_specifier(format[i], args, buffer, &index);
+		}
+		else
+		{
+			print_buffer(buffer, &index, format[i]);
+			count++;
+		}
+		i++;
+	}
 
-        va_end(args);
-        return (count);
+	print_buffer(buffer, &index, -1); /* flush remaining buffer */
+	va_end(args);
+	return (count);
 }
--- _putchar.c
+++ _putchar.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes a single character directly to stdout
  * @c: The character to print
  *
- * Return: On success 1, -1 on error
+ * Return: 1 on success, -1 on error
+ *
+ * Note: Normally, _printf uses print_buffer() instead of _putchar()
+ * to minimize write() calls. This function remains for compatibility.
  */
 int _putchar(char c)
 {
