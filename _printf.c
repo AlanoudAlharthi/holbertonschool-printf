b9d21fce354b520f6ef4eea51287c5f50ee3ce40
Ali Abdullah <aliabdullah1215@icloud.com>
2025-10-04 13:59:12 +0000

    Task 0: Implement basic _printf with %c, %s and %%

--- _printf.c
+++ _printf.c
@@ -8,45 +8,55 @@
  */
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int i = 0, count = 0;
-  char *str;
+	va_list args;
+	int i = 0, count = 0;
+	char *str;
 
-  if (!format)
-    return (-1);
+	if (format == NULL)
+		return (-1);
 
-  va_start(args, format);
+	va_start(args, format);
 
-  while (format[i])
-  {
-    if (format[i] == '%')
-    {
-      i++;
-      if (!format[i])
-        break;
-      if (format[i] == 'c')
-        count += _putchar(va_arg(args, int));
-      else if (format[i] == 's')
-      {
-        str = va_arg(args, char *);
-        if (!str)
-          str = "(null)";
-        while (*str)
-          count += _putchar(*str++);
-      }
-      else if (format[i] == '%')
-        count += _putchar('%');
-      else
-      {
-        count += _putchar('%');
-        count += _putchar(format[i]);
-      }
-    }
-    else
-      count += _putchar(format[i]);
-    i++;
-  }
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			if (format[i] == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					count += _putchar(*str);
+					str++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				count += _putchar('%');
+			}
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
 
-  va_end(args);
-  return (count);
+	va_end(args);
+	return (count);
 }
