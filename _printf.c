c550ede1da8a1455076af8a89237f2907380db61
Ali Abdullah <aliabdullah1215@icloud.com>
2025-10-04 14:07:23 +0000

    Refactor: Split _printf into smaller functions, fix Betty style in print_int

--- _printf.c
+++ _printf.c
@@ -1,6 +1,51 @@
 #include "main.h"
 
 /**
+ * print_string - prints a string
+ * @s: string to print
+ *
+ * Return: number of characters printed
+ */
+int print_string(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (*s)
+	{
+		count += _putchar(*s);
+		s++;
+	}
+	return (count);
+}
+
+/**
+ * handle_specifier - handles format specifiers
+ * @c: specifier character
+ * @args: list of arguments
+ *
+ * Return: number of characters printed
+ */
+int handle_specifier(char c, va_list args)
+{
+	int count = 0;
+
+	if (c == 'c')
+		count += _putchar(va_arg(args, int));
+	else if (c == 's')
+		count += print_string(va_arg(args, char *));
+	else if (c == '%')
+		count += _putchar('%');
+	else
+	{
+		count += _putchar('%');
+		count += _putchar(c);
+	}
+	return (count);
+}
+
+/**
  * _printf - produces output according to a format
  * @format: character string with directives
  *
@@ -10,7 +55,6 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
-	char *str;
 
 	if (format == NULL)
 		return (-1);
@@ -27,28 +71,7 @@ int _printf(const char *format, ...)
 				va_end(args);
 				return (-1);
 			}
-			if (format[i] == 'c')
-				count += _putchar(va_arg(args, int));
-			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					count += _putchar(*str);
-					str++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				count += _putchar('%');
-			}
-			else
-			{
-				count += _putchar('%');
-				count += _putchar(format[i]);
-			}
+			count += handle_specifier(format[i], args);
 		}
 		else
 		{
--- print_int.c
+++ print_int.c
@@ -8,23 +8,23 @@
  */
 int print_int(int n)
 {
-    unsigned int num;
-    int count = 0;
+	unsigned int num;
+	int count = 0;
 
-    if (n < 0)
-    {
-        count += _putchar('-');
-        num = -n;
-    }
-    else
-    {
-        num = n;
-    }
+	if (n < 0)
+	{
+		count += _putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
 
-    if (num / 10)
-        count += print_int(num / 10);
+	if (num / 10)
+		count += print_int(num / 10);
 
-    count += _putchar((num % 10) + '0');
+	count += _putchar((num % 10) + '0');
 
-    return (count);
+	return (count);
 }
