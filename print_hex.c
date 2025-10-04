b0cec99bf97d764e61247037e5ea09e2dc91620a
root <root@ip-10-42-100-77.ec2.internal>
2025-10-04 18:21:00 +0000

    Advanced: add 1024-char buffer and handle %u, %o, %x, %X

---
+++ print_hex.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_hex - prints an unsigned int in hexadecimal
+ * @n: number to print
+ * @uppercase: 1 for uppercase, 0 for lowercase
+ *
+ * Return: number of digits printed
+ */
+int print_hex(unsigned int n, int uppercase)
+{
+    int count = 0;
+    char *digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+
+    if (n / 16)
+        count += print_hex(n / 16, uppercase);
+
+    count += _putchar(digits[n % 16]);
+    return (count);
+}
